6d1f43ca3dd2cbe6d15eb8e0a0ad6d56fa713f4b
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-05-26 12:06:13 +0200

    search

--- src/components/headerApp.vue
+++ src/components/headerApp.vue
@@ -1,23 +1,42 @@
 <script>
 import axios from "axios";
-import { store } from './data/store'
+import { store } from '../data/store'
 export default {
     name: 'headerApp'
     ,
     data() {
         return {
             store,
+            apySearchMovie: 'https://api.themoviedb.org/3/search/movie',
+            searchtext: '',
+            key: '?api_key=9733f8fbead6e5ca09b6908231558d46'
+        }
+    },
+    methods: {
+        search(what) {
+            axios.get(`${what}${this.key}&query=${this.searchtext}`)
+                .then(r => {
+                    this.store.film = (r.data.results)
+                    console.log(`${this.apySearch}${this.key}&query=${this.searchtext}`)
+                    console.log(this.store.film)
+                    console.log(this.store.film[0].title)
+                })
         }
     }
 }
+
+    // created() {
+
+    // }
+
 </script>
 
 <template>
     <nav>
         <div class="container">
-            <div><input type="text">quello che cerchi</div>
+            <div><input type="text" v-model="this.searchtext">quello che cerchi</div>
             <div>
-                <button>cerca</button>
+                <button @click="search(this.apySearchMovie)">cerca</button>
             </div>
         </div>
     </nav>
